2143e9748769825b31d33e21e78fa66da777925f
sourav guha roy <souravroy94375@gmail.com>
2024-02-26 23:13:35 +0530

    Updated this file with SearchSheet component

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -6,6 +6,7 @@ import MobileNav from './MobileNav'
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from 'next/link';
+import SearchSheet from '../common/SearchSheet';
 
 export default async function Navbar() {
   const supabase = createServerComponentClient({cookies})
@@ -15,20 +16,7 @@ export default async function Navbar() {
       <div>
         <Link href={"/"}><Brand/></Link>
       </div>
-      <div className='w-full md:w-auto '>
-      <div className='hidden md:flex items-center space-x-2 border rounded-3xl p-2 hover:shadow-lg transition duration-300'>
-        <span className="text-sm pl-2">Anywhere</span>
-        <span className='text-gray-400'>|</span>
-        <span className="text-sm pl-2">Any week</span>
-        <span className='text-gray-400'>|</span>
-        <span className="text-sm pr-2 text-gray-400">Add guest</span>
-        <span className='bg-brand text-white rounded-full p-2'>
-        <Search height={15} width={15}/>
-        </span>
-      </div>
-     <MobileNav/>
-     </div>
-
+  <SearchSheet  session={data?.session?.user}/>
       <div className='flex items-center space-x-4 '>
         <Link href="/addHome"> <span className="text-sm pl-2 hidden md:flex items-center space-x-4">Add your Home</span></Link>
   
