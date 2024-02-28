f8fa5848233b5df861b6b47eefc84319eb353f17
sourav guha roy <souravroy94375@gmail.com>
2024-02-28 18:20:25 +0530

    This section defines the card for each rooms

--- src/components/common/HomeCard.tsx
+++ src/components/common/HomeCard.tsx
@@ -6,7 +6,7 @@ import { getUEImageURL } from '@/lib/utils'
 export default function HomeCard({home}:{home:any}) {
   return (
  <Link href={`/home/${home.id}`}>
-   <div>
+   <div className='mt-5'>
        <Image src={getUEImageURL(home.image)} width={100} height={100} alt={home.image} className='w-full h-[300px] rounded-xl object-cover object-center'/>
        <p className='font-semibold'>{home.city},{home.country}</p>
        <p>{home.title}</p>
