e24f38fb24044e0ba8ab37ecfe0c57d8ba1a66da
sourav guha roy <souravroy94375@gmail.com>
2024-03-16 02:26:00 +0530

    Updated the image section in the home id for the path choosing a single image

--- src/app/home/[id]/page.tsx
+++ src/app/home/[id]/page.tsx
@@ -1,12 +1,11 @@
 import React, { Suspense } from 'react'
 import FindHomeFetch from '@/components/home/idHome-fetch'
+import Loader from '@/components/loader'
 
 export default function FindHome({ params }: { params: { id: string } }) {
   return (
     <Suspense fallback={
-      <div>
-        loading ...
-      </div>}>
+      <Loader />}>
       <FindHomeFetch
         params={params}
       />
