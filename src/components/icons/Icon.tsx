ba3141351a83d54670670114f669e0e7dd9b6ba6
sourav guha roy <souravroy94375@gmail.com>
2024-03-16 02:23:51 +0530

    Changed the icons to lucid icons

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -1,9 +1,20 @@
-import { Sailboat, type IconNode as LucideIcon, Heart } from "lucide-react"
+import { Sailboat, type IconNode as LucideIcon, Heart,TentTree,Building2,Tractor,HandPlatter,SunSnow ,Flame,Coffee,LandPlot,Church,Palmtree,Wine,CableCar} from "lucide-react"
 
 export type Icon = LucideIcon
 
 export const Icons = {
     lake: Sailboat,
     heart: Heart,
-
+    camp:TentTree,
+    mansion:Building2,
+    country:Tractor,
+    luxe:HandPlatter,
+    artic:SunSnow,
+    trend:Flame,
+    coffee:Coffee,
+    golf:LandPlot,
+    church:Church,
+    beach:Palmtree,
+    wine:Wine,
+    hill:CableCar,
 }
