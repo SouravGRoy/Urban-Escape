e0eab11855bc9bc0ed03cc854d4e105a878a77a0
sourav guha roy <souravroy94375@gmail.com>
2024-03-16 02:22:58 +0530

    Updated the entire addHome section with shadcn ui and changed the handling

--- src/app/addHome/page.tsx
+++ src/app/addHome/page.tsx
@@ -6,26 +6,28 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Image from "next/image";
 import React, { Suspense } from "react";
+import Loader from "@/components/loader";
 
 export default function AddHome() {
 	return (
 		<div>
 			<ToastContainer />
 
-			<Suspense fallback={<div>loading ...</div>}>
+			<Suspense fallback={
+				<Loader />}>
 				<Navbar />
 			</Suspense>
 
 			<div className="container mt-10">
 				<div className="grid grid-cols-1 md:grid-cols-2 place-items-center md:gap-2 gap-4 mt-12">
-					<div>
-						<h1 className="text-brand font-bold text-7xl mb-3">Urban Escape</h1>
+					<div className="text-center md:text-left">
+						<h1 className="text-brand font-bold text-7xl mb-3 ">Urban Escape</h1>
 						<h1 className="text-black font-semibold text-xl mb-5">
 							The perfect Escape from your corporate Life
 						</h1>
-						<div className="flex space-x-4 items-center">
+						<div className="flex space-x-4 items-center ml-20 text-red-500 text-center">
 							<Counter num={generateRandomNum()} />
-							<strong className="text-3xl"> /per night</strong>
+							<strong className="text-3xl "> /per night</strong>
 						</div>
 						<div className="hidden md:grid grid-cols-2 mt-5">
 							<Image
