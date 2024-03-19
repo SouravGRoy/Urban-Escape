7801ac564ce3e64e3241dca7369dbc1a90fbd8cd
sourav guha roy <souravroy94375@gmail.com>
2024-03-20 01:12:21 +0530

    Updated the navMenu in order to link up to the addHome page

--- src/components/base/NavMenu.tsx
+++ src/components/base/NavMenu.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-  } from "@/components/ui/popover"
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import { MenuIcon } from 'lucide-react'
 import LoginModal from '../auth/LoginModal'
 import SignupModal from '../auth/SignupModal'
 import SignOutBtn from '../common/SignOutBtn'
 import Link from 'next/link'
-  
 
-export default function NavMenu({session}:{session:object | undefined}){
+
+export default function NavMenu({ session }: { session: object | undefined }) {
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -21,12 +21,18 @@ export default function NavMenu({session}:{session:object | undefined}){
         <ul>
           {session && (
             <>
-            <Link href="/dashboard">
-            <li className='hover:bg-gray-200 rounded-md p-2 cursor-pointer'>Dashboard</li>
-            </Link>
-             
+              <Link href="/dashboard">
+                <li className='hover:bg-gray-200  border border-green-100 rounded-md p-2 cursor-pointer'>Dashboard</li>
+              </Link>
+              <Link href="/addHome">
+                <li className="hover:bg-gray-200 border border-green-100 rounded-md p-2 cursor-pointer">
+                  Add Home
+                </li>
+              </Link>
               <SignOutBtn />
-            </> 
+
+
+            </>
           )}
           {!session && (
             <>
@@ -38,5 +44,5 @@ export default function NavMenu({session}:{session:object | undefined}){
       </PopoverContent>
     </Popover>
   );
-  
+
 }
