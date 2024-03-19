f84993641c7e45086a011394a9b6725385697969
sourav guha roy <souravroy94375@gmail.com>
2024-03-20 01:09:10 +0530

    Updated config categories in order to update the categories page

--- config/categories.ts
+++ config/categories.ts
@@ -2,8 +2,52 @@ import { CategoriesType } from "../types";
 
 export const categories: CategoriesType[] = [
   {
-    name: "Lake",
+    name: "lake",
     icon: "lake",
+  },
+  {
+    name: "camp",
+    icon: "camp",
+  },
+  {
+    name: "mansion",
+    icon: "mansion",
+  },
+  {
+    name: "country",
+    icon: "country",
+  },
+  {
+    name: "luxe",
+    icon: "luxe",
+  },
+  {
+    name: "artic",
+    icon: "artic",
+  },
+  {
+    name: "trend",
+    icon: "trend",
+  },
+  {
+    name: "coffee",
+    icon: "coffee",
+  },
+  {
+    name: "golf",
+    icon: "golf",
+  },
+  {
+    name: "church",
+    icon: "church",
+  },
+  {
+    name: "beach",
+    icon: "beach",
+  },
+  {
+    name: "wine",
+    icon: "wine"
   },
   // {
   //   name: "Farms",
