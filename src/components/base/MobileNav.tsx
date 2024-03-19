cc5a713d24873ef27be831a645eb2046fe202557
sourav guha roy <souravroy94375@gmail.com>
2024-03-20 01:11:40 +0530

    Updated the styles for mobile nav in order to have a addHome feature in mobile

--- src/components/base/MobileNav.tsx
+++ src/components/base/MobileNav.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 export default function MobileNav() {
   return (
     <div className='m-3 md:hidden '>
-     <div className='flex justify-between items-center border rounded-3xl px-5 py-1 hover:shadow-lg transition duration-300'>
-     <div className='flex items-center'>
-     <Search/>
-         <div className='flex flex-col ml-5'>
-       <span className='text-sm font-semibold'>Anywhere</span>
-       <span className='text-sm text-gray-400'>Any week ♾ Add guests</span>
-         </div>
-     </div>
-     <SlidersHorizontal/>
-     </div>
+      <div className='flex justify-between items-center border rounded-3xl px-5 py-1 hover:shadow-lg transition duration-300'>
+        <div className='flex items-center'>
+          <Search />
+          <div className='flex flex-col ml-5'>
+            <span className='text-sm font-semibold'>Anywhere</span>
+            <span className='text-sm text-gray-400'>Any week ♾ Add guests</span>
+          </div>
+        </div>
+        <SlidersHorizontal />
+      </div>
     </div>
   )
 }
