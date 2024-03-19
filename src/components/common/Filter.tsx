318b60ad7f1bb05af947d2f6cd8e9ae57f81c976
sourav guha roy <souravroy94375@gmail.com>
2024-03-20 01:16:10 +0530

    Updated the styles and responsiveness of filter

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -1,38 +1,38 @@
 import React from 'react'
 import { SlidersHorizontal } from 'lucide-react'
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 export default function Filter() {
   return (
-  <AlertDialog >
-  <AlertDialogTrigger>
-  <div className='flex mt-4 justify-between items-center border rounded-lg px-4 py-3 hover:shadow-lg space-x-3 transition duration-300'>
-    <SlidersHorizontal className='p-1 font-bold '/>
-    <h1 className='text-semibold text-md'>Filter</h1>
-    </div>
- </AlertDialogTrigger>
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle>Comming Soon ...</AlertDialogTitle>
-      <AlertDialogDescription>
-        This feature is currently in testing soon to be updated
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-    <AlertDialogFooter>
-      <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction disabled>Continue</AlertDialogAction>
-    </AlertDialogFooter>
-  </AlertDialogContent>
-</AlertDialog>    
+    <AlertDialog >
+      <AlertDialogTrigger>
+        <div className='flex mt-6 justify-between items-center border rounded-lg px-4 py-3 hover:shadow-lg space-x-3 transition duration-300'>
+          <SlidersHorizontal className='p-1 font-bold ' />
+          <h1 className='text-semibold text-md'>Filter</h1>
+        </div>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Coming Soon ...</AlertDialogTitle>
+          <AlertDialogDescription>
+            This feature is currently in testing soon to be updated
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
