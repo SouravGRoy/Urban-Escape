d4350fea9c1ec93ebffe8c5e0ea41d0afc5277c2
sourav guha roy <souravroy94375@gmail.com>
2024-03-21 00:26:33 +0530

    Updated the rendering of multiple images now

--- src/components/home/idHome-fetch.tsx
+++ src/components/home/idHome-fetch.tsx
@@ -5,13 +5,13 @@ import Navbar from "@/components/base/Navbar";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { capitalizeFirstLetter, getUEImageURL } from "@/lib/utils";
 import { HomeType } from "../../../types";
+import { notFound } from "next/navigation";
 
 interface PageProps {
 	params: {
 		id: string;
 	};
 }
-
 export default async function FindHomeFetch({ params }: PageProps) {
 	const cookieStore = cookies();
 	const supabase = createServerComponentClient({
@@ -22,6 +22,9 @@ export default async function FindHomeFetch({ params }: PageProps) {
 		.select("*,users(metadata->name)")
 		.eq("id", params.id);
 	const home: HomeType | null = data?.[0];
+	if (!home) notFound()
+
+	const tiles = generateTiles(JSON.parse(home?.image));
 	return (
 		<div className="mb-10">
 			<Navbar />
@@ -35,57 +38,33 @@ export default async function FindHomeFetch({ params }: PageProps) {
 					</div>
 
 					<div className="flex flex-wrap justify-center items-center rounded-lg overflow-hidden w-full">
-						{/* Original Size Image */}
 						<div className="flex flex-wrap w-full md:w-1/2 ">
 							<div className="w-full md:w-full md:pr-2 md:h-[465px] h-[300px]">
 								<Image
 									alt="gallery"
-									className="w-full h-[350px] md:h-full object-cover object-center block rounded-lg"
-									src={getUEImageURL(home?.image)}
+									className="w-full h-[350px] md:h-full object-cover object-center block rounded-lg rounded-r-none"
+									src={tiles[0].url}
 									width={500}
 									height={300}
 								/>
 							</div>
+
 						</div>
 
-						{/* Resized Images */}
-						<div className="flex-wrap w-1/2 hidden md:flex">
-							<div className="h-[230px] pr-2 mb-1 w-1/2 md:w-1/2">
-								<Image
-									alt="gallery"
-									className="w-full object-cover h-full object-center block"
-									src={getUEImageURL(home?.image)}
-									width={250}
-									height={100}
-								/>
-							</div>
-							<div className="h-[230px] w-1/2 md:w-1/2">
-								<Image
-									alt="gallery"
-									className="w-full object-cover h-full object-center block"
-									src={getUEImageURL(home?.image)}
-									width={250}
-									height={100}
-								/>
-							</div>
-							<div className=" h-[230px] pr-2 w-1/2 md:w-1/2 ">
-								<Image
-									alt="gallery"
-									className="w-full h-full object-cover object-center block"
-									src={getUEImageURL(home?.image)}
-									width={250}
-									height={100}
-								/>
-							</div>
-							<div className=" h-[230px] w-1/2 md:w-1/2">
-								<Image
-									alt="gallery"
-									className="w-full h-full object-cover  object-center block"
-									src={getUEImageURL(home?.image)}
-									width={250}
-									height={100}
-								/>
-							</div>
+						<div className="grid-cols-2 gap-2 w-1/2 hidden md:grid">
+							{
+								tiles.slice(1, 5).map((_, i) => (
+									<div className=" h-[230px] w-full md:w-full" key={i}>
+										<Image
+											alt="gallery"
+											className="w-full h-full object-cover  object-center block"
+											src={tiles[i].url}
+											width={250}
+											height={100}
+										/>
+									</div>
+								))
+							}
 						</div>
 					</div>
 
@@ -106,3 +85,17 @@ export default async function FindHomeFetch({ params }: PageProps) {
 		</div>
 	);
 }
+interface Tile {
+	url: string;
+	name: string;
+}
+
+function generateTiles(url?: string[]): Tile[] {
+	if (!url?.length) return [];
+	return url.map((u) => ({
+		url: getUEImageURL(u),
+		name: "gallery",
+	}));
+}
+
+
