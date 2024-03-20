792f820c0ecc6ff194a211e6c80e266a84a658f6
sourav guha roy <souravroy94375@gmail.com>
2024-03-21 00:25:49 +0530

    Changed the styling of the home fetch to change from home card grid

--- src/components/home/Home-fetch.tsx
+++ src/components/home/Home-fetch.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Navbar from "@/components/base/Navbar";
 import { Categories } from "@/components/common/Categories";
 import HomeCard from "@/components/common/HomeCard";
@@ -7,38 +7,44 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 interface PageProps {
-    searchParams?: {
-        country: string,
-        category: string,
-    }
+  searchParams?: {
+    country: string;
+    category: string;
+  };
 }
 
 export default async function Homefetch({ searchParams }: PageProps) {
-    const cookieStore = cookies();
-	const supabase = createServerComponentClient({
-		cookies: () => cookieStore,
-	});
-    const query = supabase
-        .from("homes").select("id,title,image,country,city,price,users(metadata->name)")
-    if (searchParams?.country) {
-        query.ilike('country', `%${searchParams?.country}%`)
-    }
-    if (searchParams?.category) {
-        query.contains('categories', [searchParams?.category])
-    }
-    const { data: homes, error } = await query
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({
+    cookies: () => cookieStore,
+  });
+  const query = supabase
+    .from("homes")
+    .select("id,title,image,country,city,price,users(metadata->name)");
+  if (searchParams?.country) {
+    query.ilike("country", `%${searchParams?.country}%`);
+  }
+  if (searchParams?.category) {
+    query.contains("categories", [searchParams?.category]);
+  }
+  const { data: homes, error } = await query;
 
-
-    return (
-        <div>
-            <Toast />
-            <Navbar />
-            <Categories />
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 px-10">
-                {homes && homes.length > 0 && homes.map((item) => <HomeCard home={item} key={item.id} />)}
-            </div>
-            {homes && homes.length <= 0 && <h1 className="text-brand font-bold mt-5 text-2xl text-center">No UE Found</h1>
-            }
-        </div>
-    );
+  return (
+    <div>
+      <Toast />
+      <Navbar />
+      <Categories />
+      <hr className="h-px mb-1 bg-gray-100 border-0 dark:bg-gray-500"></hr>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 px-5">
+        {homes &&
+          homes.length > 0 &&
+          homes.map((item) => <HomeCard home={item} key={item.id} />)}
+      </div>
+      {homes && homes.length <= 0 && (
+        <h1 className="text-brand font-bold mt-5 text-2xl text-center">
+          No UE Found
+        </h1>
+      )}
+    </div>
+  );
 }
