813c119cead9654d1997d65512041992a86c9274
sourav guha roy <souravroy94375@gmail.com>
2024-03-31 12:01:57 +0530

    Changes in the logo to make it mobile responsive

--- src/components/base/Brand.tsx
+++ src/components/base/Brand.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 export default function Brand() {
   return (
     <div>
-        {/* image from images */}
-        <Image src="/images/logo.png" alt="logo" width={140} height={140} className='hidden lg:block'/>
-        {/* image from small screen */}
-        <Image src="/images/mobileLogo.png" alt="logo" width={90} height={90} className='lg:hidden'/>
+      {/* image from images */}
+      <Image
+        src="/images/logo.png"
+        alt="logo"
+        width={140}
+        height={140}
+        className="hidden lg:block"
+      />
+      {/* image from small screen */}
+      <Image
+        src="/images/mobileLogo.png"
+        alt="logo"
+        width={90}
+        height={90}
+        className="lg:hidden"
+      />
     </div>
-  )
+  );
 }
