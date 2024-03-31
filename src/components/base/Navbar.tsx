0726ac10f3b1d839a4a66834ec056ddfd187b50a
sourav guha roy <souravroy94375@gmail.com>
2024-03-31 12:03:02 +0530

    Update in the position and styles

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -6,33 +6,33 @@ import Brand from "./Brand";
 import NavMenu from "./NavMenu";
 
 export default async function Navbar() {
-	const cookieStore = cookies();
-	const supabase = createServerComponentClient({
-		cookies: () => cookieStore,
-	});
-	const { data, error } = await supabase.auth.getSession();
-	return (
-		<div className="flex items-center justify-between md:py-3  px-4 md:px-10 border-b-[1px]">
-			<div className="hidden md:block">
-				<Link href={"/"}>
-					<Brand />
-				</Link>
-			</div>
-			<div className="w-full md:w-auto">
-				<SearchSheet session={data?.session?.user} />
-			</div>
-			<div className="flex items-center space-x-4 ">
-				<Link href="/addHome">
-					{" "}
-					<span className="hover:bg-green-100  border border-green-100 rounded-full p-2 cursor-pointer text-sm pl-2 hidden md:flex items-center space-x-4">
-						Add your Home
-					</span>
-				</Link>
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({
+    cookies: () => cookieStore,
+  });
+  const { data, error } = await supabase.auth.getSession();
+  return (
+    <div className="flex items-center justify-between md:py-3 px-1 md:px-10 border-b-[1px]">
+      <div className="hidden md:block">
+        <Link href={"/"}>
+          <Brand />
+        </Link>
+      </div>
+      <div className="w-full md:w-auto">
+        <SearchSheet session={data?.session?.user} />
+      </div>
+      <div className="flex items-center space-x-2 ">
+        <Link href="/addHome">
+          {" "}
+          <span className="hover:bg-green-100  border border-green-100 rounded-full p-2 cursor-pointer text-sm pl-2 hidden md:flex items-center space-x-4">
+            Add your Home
+          </span>
+        </Link>
 
-				<div className="border p-2 rounded-full hover:shadow-lg transition duration-300 ">
-					<NavMenu session={data?.session?.user} />
-				</div>
-			</div>
-		</div>
-	);
+        <div className="border p-2 rounded-full hover:shadow-lg transition duration-300 ">
+          <NavMenu session={data?.session?.user} />
+        </div>
+      </div>
+    </div>
+  );
 }
