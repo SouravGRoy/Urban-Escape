bd04bc960f71286203e687c02c5b35a3ca481d3f
sourav guha roy <souravroy94375@gmail.com>
2024-04-20 16:41:19 +0530

    Updated styles and function

--- src/components/common/HomeCard.tsx
+++ src/components/common/HomeCard.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { getUEImageURL, parseJSON } from "@/lib/utils";
+"use client";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
+import { getUEImageURL, parseJSON } from "@/lib/utils";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Tile {
   url: string;
@@ -17,60 +17,51 @@ interface Tile {
 
 export default function HomeCard({ home }: { home: any }) {
   return (
-    <>
-      <div className="container mt-4 flex justify-center">
+    <Link href={`/home/${home.id}`}>
+      <div className="mt-4 flex flex-col justify-center group shadow-lg shadow-cyan-300/40 rounded-tl-3xl overflow-hidden">
         <Carousel
           opts={{
             align: "start",
             loop: true,
           }}
-          className="group transition duration-700 ease-in-out lg:w-[282px] ax-w-[350px] w-full shadow-lg shadow-cyan-300/40"
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
         >
-          <CarouselContent className="aspect-w-16 aspect-h-9 ">
+          <CarouselContent>
             {parseJSON<string[]>(home.image).map((image, index) => (
-              <CarouselItem className=" w-full px-4" key={index}>
-                <Link
-                  className="z-0 w-full h-full aspect-square"
-                  href={`/home/${home.id}`}
-                >
-                  <div className="w-[350px] rounded-xl">
-                    <Image
-                      className="h-[267px] w-full object-cover object-center rounded-t-3xl "
-                      width={354}
-                      height={100}
-                      src={getUEImageURL(image)}
-                      alt={"image"}
-                    />
-                  </div>
-
-                  <div>
-                    <div className="w-full max-w-[282px] sm:max-w-none">
-                      <div className="flex md:mt-2 px-1 justify-between">
-                        <div>
-                          <p className="font-semibold">
-                            {home.city}, {home.country}
-                          </p>
-                          <p className="font-bold text-gray-400">
-                            {home.title}
-                          </p>
-                        </div>
-
-                        <div className="">
-                          <p className="font-semibold">Rs. {home.price}</p>
-                        </div>
-                      </div>
+              <CarouselItem key={index}>
+                <Card className="p-0">
+                  <CardContent className="p-0">
+                    <div className="aspect-square rounded-md overflow-hidden">
+                      <Image
+                        className="object-cover w-full h-full"
+                        src={getUEImageURL(image)}
+                        width={500}
+                        height={500}
+                        alt="image"
+                      />
                     </div>
-                  </div>
-                </Link>
+                  </CardContent>
+                </Card>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <div className="hidden group-hover:flex ">
-            <CarouselPrevious className="z-[9999] -left-0 top-1/2 -translate-y-1/2" />
-            <CarouselNext className="z-[9999] -right-0 top-1/2 -translate-y-1/2" />
-          </div>
         </Carousel>
+        <div className="flex px-1 mt-2 justify-between">
+          <div>
+            <p className="font-semibold">
+              {home.city}, {home.country}
+            </p>
+            <p className="font-bold text-gray-400">{home.title}</p>
+          </div>
+          <div className="">
+            <p className="font-semibold">Rs. {home.price}</p>
+          </div>
+        </div>
       </div>
-    </>
+    </Link>
   );
 }
