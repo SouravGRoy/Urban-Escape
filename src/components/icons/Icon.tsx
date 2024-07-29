e3b5ee1ce324095dd9f2736c8a9c963f511c9d69
sourav guha roy <souravroy94375@gmail.com>
2024-07-30 00:27:36 +0530

    Icons package installed

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -1,20 +1,36 @@
-import { Sailboat, type IconNode as LucideIcon, Heart,TentTree,Building2,Tractor,HandPlatter,SunSnow ,Flame,Coffee,LandPlot,Church,Palmtree,Wine,CableCar} from "lucide-react"
+import {
+  Sailboat,
+  type IconNode as LucideIcon,
+  Heart,
+  TentTree,
+  Building2,
+  Tractor,
+  HandPlatter,
+  SunSnow,
+  Flame,
+  Coffee,
+  LandPlot,
+  Church,
+  Palmtree,
+  Wine,
+  CableCar,
+} from "lucide-react";
 
-export type Icon = LucideIcon
+export type Icon = LucideIcon;
 
 export const Icons = {
-    lake: Sailboat,
-    heart: Heart,
-    camp:TentTree,
-    mansion:Building2,
-    country:Tractor,
-    luxe:HandPlatter,
-    artic:SunSnow,
-    trend:Flame,
-    coffee:Coffee,
-    golf:LandPlot,
-    church:Church,
-    beach:Palmtree,
-    wine:Wine,
-    hill:CableCar,
-}
+  lake: Sailboat,
+  heart: Heart,
+  camp: TentTree,
+  mansion: Building2,
+  country: Tractor,
+  luxe: HandPlatter,
+  artic: SunSnow,
+  trend: Flame,
+  coffee: Coffee,
+  golf: LandPlot,
+  church: Church,
+  beach: Palmtree,
+  wine: Wine,
+  hill: CableCar,
+};
